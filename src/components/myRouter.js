afbec7604f870b1133bd75d5aff2508ff80cd41a
Jjsue <jjsue_95@hotmail.es>
2020-09-07 10:26:40 +0200

    Cambios en HREF

--- src/components/myRouter.js
+++ src/components/myRouter.js
@@ -16,7 +16,7 @@ export default class MyRouter extends Component {
             <>
                 <Router>
                     <Navbar variant="dark" bg="dark" expand="lg">
-                        <Navbar.Brand href="/app">Proyecto Contemplador</Navbar.Brand>
+                        <Navbar.Brand href="/">Proyecto Contemplador</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
